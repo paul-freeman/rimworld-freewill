b0a4e9a0b0de2f9965745d0529da10a3853508f4
Paul Freeman <paul.freeman.cs@gmail.com>
2021-08-17 23:46:04 +1200

    finish first playable draft of mod

--- ThoughtWorkers/EnforcedWorkSchedule.cs
+++ ThoughtWorkers/EnforcedWorkSchedule.cs
@@ -6,8 +6,12 @@ public class ThoughtWorker_Precept_EnforcedWorkSchedule : ThoughtWorker_Precept
 {
     protected override ThoughtState ShouldHaveThought(Pawn pawn)
     {
-        var worldComp = Find.World.GetComponent<FreeWill_WorldComponent>();
-        // free pawns should not have this thought
-        return !worldComp.HasFreeWill(pawn);
+        if (pawn.IsColonistPlayerControlled || pawn.IsSlaveOfColony)
+        {
+            var worldComp = Find.World.GetComponent<FreeWill_WorldComponent>();
+            // free pawns should not have this thought
+            return !worldComp.HasFreeWill(pawn);
+        }
+        return false;
     }
 }
--- ThoughtWorkers/NoWorkSchedule.cs
+++ ThoughtWorkers/NoWorkSchedule.cs
@@ -6,8 +6,12 @@ public class ThoughtWorker_Precept_NoWorkSchedule : ThoughtWorker_Precept
 {
     protected override ThoughtState ShouldHaveThought(Pawn pawn)
     {
-        var worldComp = Find.World.GetComponent<FreeWill_WorldComponent>();
-        // free pawns should have this thought
-        return worldComp.HasFreeWill(pawn);
+        if (pawn.IsColonistPlayerControlled || pawn.IsSlaveOfColony)
+        {
+            var worldComp = Find.World.GetComponent<FreeWill_WorldComponent>();
+            // free pawns should have this thought
+            return worldComp.HasFreeWill(pawn);
+        }
+        return false;
     }
 }
